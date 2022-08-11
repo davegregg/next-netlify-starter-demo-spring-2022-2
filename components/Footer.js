2cc0f332b3cdb995cf5e83ba66ce62668b6d7489
davegregg <davegregg@gmail.com>
2022-08-11 11:57:32 -0400

    Add hello world text to bottom of page

--- components/Footer.js
+++ components/Footer.js
@@ -6,6 +6,8 @@ export default function Footer() {
       <footer className={styles.footer}>
         Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
       </footer>
+
+      <h3>Hello world! DMG is demoing a change to the deployment!</h3>
     </>
   )
 }
