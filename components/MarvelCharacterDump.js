528e59ff6897e73e77267cbc6c5e6de6cddd0401
davegregg <davegregg@gmail.com>
2022-08-11 13:26:13 -0400

    Fix deploy url for marvel function endpoint

--- components/MarvelCharacterDump.js
+++ components/MarvelCharacterDump.js
@@ -4,7 +4,7 @@ export default function MarvelCharacterDump () {
     const [character, setCharacter] = useState({})
 
     useEffect(() => {
-        fetch("http://localhost:53498/marvel?searchTerm=spider&limit=100&offset=1")
+        fetch("/.netlify/functions/marvel?searchTerm=spider&limit=100&offset=1")
             .then(response => response.json())
             .then(characters => characters[0])
             .then(selectedCharacter => setCharacter(selectedCharacter))
